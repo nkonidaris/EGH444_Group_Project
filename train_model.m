bd8269faa08b124883faabd878d99195129ef03f
Nicholas K <nkonidaris@outlook.com>
2021-10-24 01:17:08 +0200

    First Model Training

--- train_model.m
+++ train_model.m
@@ -38,15 +38,11 @@ imageDatastore.ReadFcn = @customReadDatastoreImage;
 
 %% Reworking model
 
-% Visulise Network
-deepNetworkDesigner(net)
-
 % 244 by 224 by 3 (244x244 RGB Image)
 inputSize = net.Layers(1).InputSize;
 
 % Extract layer graph
 lgraph = layerGraph(net); 
-[learnableLayer,classLayer] = findLayersToReplace(lgraph);
 
 % Get number training classes
 numClasses = numel(categories(imdsTrain.Labels));
@@ -65,6 +61,11 @@ lgraph = replaceLayer(lgraph,'loss3-classifier',newLearnableLayer);
 newClassLayer = classificationLayer('Name','Classification Bridge');
 lgraph = replaceLayer(lgraph,'output',newClassLayer);
 
+%% Visulise Network
+%deepNetworkDesigner(net)
+
+plot(lgraph);
+
 
 %% Image Augmentation, increase image dataset
 
@@ -73,7 +74,7 @@ imageAugmenter = imageDataAugmenter( ...
     'RandXReflection',true, ...
     'RandYReflection',true, ...
     'RandRotation', [-45 45], ...
-    'RandScale', [0.5 4], ...
+    'RandScale', [1 2], ...
     'RandXTranslation',pixelRange, ...
     'RandYTranslation',pixelRange);
 
@@ -85,17 +86,22 @@ augimdsValidation = augmentedImageDatastore(inputSize(1:2),imdsValidation);
 
 %% Train model
 
-options = trainingOptions('sgdm', ...
+options = trainingOptions('adam', ...
     'MiniBatchSize',10, ...
-    'MaxEpochs',6, ...
+    'MaxEpochs',30, ...
     'InitialLearnRate',1e-4, ...
     'Shuffle','every-epoch', ...
     'ValidationData',augimdsValidation, ...
     'ValidationFrequency',3, ...
     'Verbose',false, ...
-    'Plots','training-progress');
+    'Plots','training-progress',...
+    'ExecutionEnvironment', 'parallel');
 
 netTransfer = trainNetwork(augimdsTrain,lgraph,options);
+
+%% Save model for 
+
+save netTransfer
 
 %%
 
@@ -243,7 +249,7 @@ options = trainingOptions('adam', ...
 
 netTransfer3 = trainNetwork(augimdsTrain,layers,options);
 
-%%
+%% Custom functions used on script
 
 function data = customReadDatastoreImage(filename)
 
@@ -252,57 +258,6 @@ data = imread(filename);
 % Half images get gaussian noise
 if rand > 0.5
 data = imnoise(data, 'gaussian');
-end
-
-end
-
-% findLayersToReplace(lgraph) finds the single classification layer and the
-% preceding learnable (fully connected or convolutional) layer of the layer
-% graph lgraph.
-function [learnableLayer,classLayer] = findLayersToReplace(lgraph)
-
-if ~isa(lgraph,'nnet.cnn.LayerGraph')
-    error('Argument must be a LayerGraph object.')
-end
-
-% Get source, destination, and layer names.
-src = string(lgraph.Connections.Source);
-dst = string(lgraph.Connections.Destination);
-layerNames = string({lgraph.Layers.Name}');
-
-% Find the classification layer. The layer graph must have a single
-% classification layer.
-isClassificationLayer = arrayfun(@(l) ...
-    (isa(l,'nnet.cnn.layer.ClassificationOutputLayer')|isa(l,'nnet.layer.ClassificationLayer')), ...
-    lgraph.Layers);
-
-if sum(isClassificationLayer) ~= 1
-    error('Layer graph must have a single classification layer.')
-end
-classLayer = lgraph.Layers(isClassificationLayer);
-
-
-% Traverse the layer graph in reverse starting from the classification
-% layer. If the network branches, throw an error.
-currentLayerIdx = find(isClassificationLayer);
-while true
-    
-    if numel(currentLayerIdx) ~= 1
-        error('Layer graph must have a single learnable layer preceding the classification layer.')
-    end
-    
-    currentLayerType = class(lgraph.Layers(currentLayerIdx));
-    isLearnableLayer = ismember(currentLayerType, ...
-        ['nnet.cnn.layer.FullyConnectedLayer','nnet.cnn.layer.Convolution2DLayer']);
-    
-    if isLearnableLayer
-        learnableLayer =  lgraph.Layers(currentLayerIdx);
-        return
-    end
-    
-    currentDstIdx = find(layerNames(currentLayerIdx) == dst);
-    currentLayerIdx = find(src(currentDstIdx) == layerNames);
-    
 end
 
 end
