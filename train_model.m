2665f1e7af2a85e8e7a4414a696c4b0def3f42cb
Nicholas K <nkonidaris@outlook.com>
2021-10-24 00:09:16 +0200

    Training new model

--- train_model.m
+++ train_model.m
@@ -1,27 +1,120 @@
+% EGH444 - Group Project 
+% by Nicholas Konidaris & Thomas Cotter
+
+% Clear all
+clear all; close all, clc
+%% Importing
+
+% Load pretrained: GoogLeNet
 net = googlenet;
 
+% Load images into datastore
+imds = imageDatastore('Training_Data\Level 1\', ...
+    'IncludeSubfolders',true, ...
+    'LabelSource','foldernames');
+
+imageDatastore.ReadFcn = @customReadDatastoreImage;
+
+%% Manual image resizing on image reading
+
+ % Read image 
+% I = readimage(imds,3);
+% s = size(I);
+% fprintf("Size of original images vary, e.g. First indexed image size: %d by %d\n",s(1),s(2));
+ 
+ % Resize images for use in pretrained model
+% inputSize = net.Layers(1).InputSize;
+% imds.ReadFcn = @(loc)imresize(imread(loc),inputSize(1:2));
+ 
+ % Read resized image 
+% I_new = readimage(imds,3);
+% imshowpair(I, I_new,'montage');
+% s = size(I_new);
+% fprintf("Size of new images: %d by %d\n",s(1),s(2));
+
+%% 70/30 split for training / validation, ramdonized
+
+[imdsTrain,imdsValidation] = splitEachLabel(imds,0.7,'randomized');
+
+%% Reworking model
+
+% Visulise Network
+deepNetworkDesigner(net)
+
+% 244 by 224 by 3 (244x244 RGB Image)
 inputSize = net.Layers(1).InputSize;
 
+% Extract layer graph
 lgraph = layerGraph(net); 
 [learnableLayer,classLayer] = findLayersToReplace(lgraph);
 
+% Get number training classes
 numClasses = numel(categories(imdsTrain.Labels));
 
-if isa(learnableLayer,'nnet.cnn.layer.FullyConnectedLayer')
-    newLearnableLayer = fullyConnectedLayer(numClasses, ...
-        'Name','new_fc', ...
-        'WeightLearnRateFactor',10, ...
-        'BiasLearnRateFactor',10);
+newLearnableLayer = fullyConnectedLayer(numClasses, ...
+    'Name','FC Bridge Layer', ...
+    'WeightLearnRateFactor',10, ...
+    'BiasLearnRateFactor',10);
     
-elseif isa(learnableLayer,'nnet.cnn.layer.Convolution2DLayer')
-    newLearnableLayer = convolution2dLayer(1,numClasses, ...
-        'Name','new_conv', ...
-        'WeightLearnRateFactor',10, ...
-        'BiasLearnRateFactor',10);
+% Replace fully connected layer for Bridge learning
+lgraph = replaceLayer(lgraph,'loss3-classifier',newLearnableLayer);
+
+% Leave softmax layer for classification output
+
+% Replace classifcation layer for new outputs
+newClassLayer = classificationLayer('Name','Classification Bridge');
+lgraph = replaceLayer(lgraph,'output',newClassLayer);
+
+
+%% Image Augmentation, increase image dataset
+
+pixelRange = [-30 30];
+imageAugmenter = imageDataAugmenter( ...
+    'RandXReflection',true, ...
+    'RandYReflection',true, ...
+    'RandRotation', [-45 45], ...
+    'RandScale', [0.5 4], ...
+    'RandXTranslation',pixelRange, ...
+    'RandYTranslation',pixelRange);
+
+augimdsTrain = augmentedImageDatastore(inputSize(1:2),imdsTrain, ...
+    'DataAugmentation',imageAugmenter);
+augimdsValidation = augmentedImageDatastore(inputSize(1:2),imdsValidation);
+
+% Custom read function used to add random noise to images
+
+%% Train model
+
+options = trainingOptions('sgdm', ...
+    'MiniBatchSize',10, ...
+    'MaxEpochs',6, ...
+    'InitialLearnRate',1e-4, ...
+    'Shuffle','every-epoch', ...
+    'ValidationData',augimdsValidation, ...
+    'ValidationFrequency',3, ...
+    'Verbose',false, ...
+    'Plots','training-progress');
+
+netTransfer = trainNetwork(augimdsTrain,lgraph,options);
+
+%%
+
+[YPred,scores] = classify(netTransfer,augimdsValidation);
+
+idx = randperm(numel(imdsValidation.Files),4);
+figure
+for i = 1:4
+    subplot(2,2,i)
+    I = readimage(imdsValidation,idx(i));
+    imshow(I)
+    label = YPred(idx(i));
+    title(string(label));
 end
 
-lgraph = replaceLayer(lgraph,learnableLayer.Name,newLearnableLayer);
+YValidation = imdsValidation.Labels;
+accuracy = mean(YPred == YValidation)
 
+%%
 newClassLayer = classificationLayer('Name','new_classoutput');
 lgraph = replaceLayer(lgraph,classLayer.Name,newClassLayer);
 
@@ -149,3 +242,68 @@ options = trainingOptions('adam', ...
     'Plots','training-progress');
 
 netTransfer3 = trainNetwork(augimdsTrain,layers,options);
+
+%%
+
+function data = customReadDatastoreImage(filename)
+
+data = imread(filename);
+
+% Half images get gaussian noise
+if rand > 0.5
+data = imnoise(data, 'gaussian');
+end
+
+end
+
+% findLayersToReplace(lgraph) finds the single classification layer and the
+% preceding learnable (fully connected or convolutional) layer of the layer
+% graph lgraph.
+function [learnableLayer,classLayer] = findLayersToReplace(lgraph)
+
+if ~isa(lgraph,'nnet.cnn.LayerGraph')
+    error('Argument must be a LayerGraph object.')
+end
+
+% Get source, destination, and layer names.
+src = string(lgraph.Connections.Source);
+dst = string(lgraph.Connections.Destination);
+layerNames = string({lgraph.Layers.Name}');
+
+% Find the classification layer. The layer graph must have a single
+% classification layer.
+isClassificationLayer = arrayfun(@(l) ...
+    (isa(l,'nnet.cnn.layer.ClassificationOutputLayer')|isa(l,'nnet.layer.ClassificationLayer')), ...
+    lgraph.Layers);
+
+if sum(isClassificationLayer) ~= 1
+    error('Layer graph must have a single classification layer.')
+end
+classLayer = lgraph.Layers(isClassificationLayer);
+
+
+% Traverse the layer graph in reverse starting from the classification
+% layer. If the network branches, throw an error.
+currentLayerIdx = find(isClassificationLayer);
+while true
+    
+    if numel(currentLayerIdx) ~= 1
+        error('Layer graph must have a single learnable layer preceding the classification layer.')
+    end
+    
+    currentLayerType = class(lgraph.Layers(currentLayerIdx));
+    isLearnableLayer = ismember(currentLayerType, ...
+        ['nnet.cnn.layer.FullyConnectedLayer','nnet.cnn.layer.Convolution2DLayer']);
+    
+    if isLearnableLayer
+        learnableLayer =  lgraph.Layers(currentLayerIdx);
+        return
+    end
+    
+    currentDstIdx = find(layerNames(currentLayerIdx) == dst);
+    currentLayerIdx = find(src(currentDstIdx) == layerNames);
+    
+end
+
+end
+
